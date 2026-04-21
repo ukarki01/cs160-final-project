2fb77079d8d9a384d2659e9086e6b46225547e36
Uma Karki <70490205+ukarki01@users.noreply.github.com>
2026-04-21 12:56:11 -0700

    Update urls.py

--- p1/urls.py
+++ p1/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/salidify/')),  # add this line
     path('salidify/', include('salidify.urls')),
     path('admin/', admin.site.urls),
 ]
