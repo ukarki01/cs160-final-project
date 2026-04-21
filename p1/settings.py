7037e58e3b9e3b420b2c185b5cbf9487b54c53e5
Uma Karki <70490205+ukarki01@users.noreply.github.com>
2026-04-21 12:58:29 -0700

    Update settings.py

--- p1/settings.py
+++ p1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dh)m$rf)%qu#%pvyo=7b8gipl6p$i%v6l=_-mxoja$ylc!4rny'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
